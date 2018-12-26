cc1a10bd09391e7db56bbca613aa02de27501a17
alx <37true@gmail.com>
2018-12-27 06:46:51 +0700

    second activity added

--- app/src/main/java/tk/alltrue/manual/MainActivity.java
+++ app/src/main/java/tk/alltrue/manual/MainActivity.java
@@ -1,7 +1,12 @@
 package tk.alltrue.manual;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +25,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+
+        listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, titles));
+        listView.setTextFilterEnabled(true);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, DetailActivity.class);
+                intent.putExtra("title", position);
+                startActivity(intent);
+            }
+        });
     }
 }
